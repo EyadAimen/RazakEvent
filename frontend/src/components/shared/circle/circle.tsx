7d95635095ae0d051b58e6992dcd69de8a6fc47c
Abdalla-Ali3 <3bda3llah3@gmail.com>
2026-05-04 18:29:24 +0800

    resuable objects for sign up page

--- frontend/src/components/shared/circle/circle.tsx
+++ frontend/src/components/shared/circle/circle.tsx
@@ -1,10 +1,10 @@
-import styles from "./floatingObjects.module.css";
+import styles from "./circle.module.css";
 
 type Props = {
     style?: React.CSSProperties;
 };
 
-export default function FloatingCircle({ style }: Props) {
+export default function Circle({ style }: Props) {
     return (
         <div className={`${styles.shape} ${styles.circle}`}
             style={style}></div>
